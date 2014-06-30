4d1ee64c4e845ef2721af03d242639a43f60ff87
liaohuqiu <liaohuqiu@gmail.com>
2014-06-30 19:04:06 +0800

    upgrade versio

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'cubi',
-    version = '0.0.4',
+    version = '0.0.5',
     keywords = ('cube', 'cubi', 'cube-rpc'),
     description = 'python implementation for Cube RPC protocol',
     license = 'MIT License',
