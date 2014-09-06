717c854fc7cc692be490d66c755e77f97466e4bf
liaohqiu <liaohuqiu@gmail.com>
2014-09-06 09:46:31 +0800

    update test

--- test-block-io.py
+++ test-block-io.py
@@ -6,18 +6,28 @@ gevent.monkey.patch_all(thread=False)
 import cubi.proxy as proxy
 import cubi.logger as logger
 
-if __name__ == '__main__':
+import threading
 
-    logger.enable_debug_log()
-
-    endp = 'placeholderServer@tcp::16512'
+def test():
+    endp = 'placeholderServer@tcp:10.97.240.139:16512'
     print 'start test for ', endp
     prx = proxy.Proxy(endp, True)
 
-    data = {'time': "xxx"}
-    r = prx.request_blocking('r', data)
-    print(r)
+    data = {}
+    while (True):
+        r = prx.request_blocking('r', data)
 
-    prx.start();
-    r = prx.request('r', data)
-    print(r)
+if __name__ == '__main__':
+
+    # logger.enable_debug_log()
+
+    ts = []
+    for i in range(1, 3):
+        print(i);
+        t = threading.Thread(target=test, args = ())
+        t.start()
+        ts.append(t)
+
+    for t in ts:
+        t.join()
+
